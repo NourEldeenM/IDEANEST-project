fc315381c89bbbf0d6f44ca5dc76aa8b14906bf4
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-10-25 18:03:22 +0300

    remove dist files

--- dist/code/config/mongoose.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const mongoose_1 = require("mongoose");
-const mongoose = (0, mongoose_1.connect)("mongodb+srv://nourislight:nour1324@cluster0.nwykl.mongodb.net/ideanest?retryWrites=true&w=majority&appName=Cluster0").then(() => {
-    console.log("MongoDB connected successfully");
-});
-module.exports = mongoose;
